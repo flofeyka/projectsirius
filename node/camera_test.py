b82da8d5c734b5bfb5609641ab303f805d57dc9d
Dmitry <130653856+Fortunny927@users.noreply.github.com>
2023-11-22 15:04:11 +0300

    Update camera_test.py

--- node/camera_test.py
+++ node/camera_test.py
@@ -1,10 +1,35 @@
 import cv2
+import time
+import requests
+import numpy as np
 
+frame_list = []
 
-cap = cv2.VideoCapture(0)
-ret, frame = cap.read()
-cap.release()
+while True:
+    cap = cv2.VideoCapture(1)
+    ret, frame = cap.read()
+    cap.release()
 
-cv2.imwrite('my_image.jpg', frame)
+    # Сохраняем текущий кадр
+    cv2.imwrite('current_frame.jpg', frame)
 
-print("Изображение сохранено.")
+    # Добавляем текущий кадр в список
+    frame_list.append(frame)
+
+    # Если в списке уже есть 3 кадра, удаляем самый старый
+    if len(frame_list) > 3:
+        frame_list.pop(0)
+
+    # Сравниваем текущий кадр с предыдущими двумя
+    diff_count = 0
+    for i in range(len(frame_list) - 1):
+        similarity = cv2.matchTemplate(frame_list[i], frame_list[i+1], cv2.TM_CCOEFF_NORMED)
+        if np.max(similarity) < 0.65:  # Пороговое значение для определения несовпадения
+            diff_count += 1
+
+    # Если текущий кадр не совпадает на 65 процентов с предыдущими двумя, отправляем "FALSE" на сервер
+    if diff_count > 0:
+        response = requests.get("http://localhost:5000/cam")
+        print(response.text)
+
+    time.sleep(1)
